f556294de57b5c371ad965d2b6c7e470109cbd92
Oskarzito <oskar.kv@gmail.com>
2017-12-08 11:40:12 +0100

    Initial commit

---
+++ src/System.cpp
@@ -0,0 +1,55 @@
+//
+// Created by Oskar Emilsson on 2017-11-30.
+//
+
+#include "System.h"
+#include <string>
+
+namespace game {
+
+    System::System() {
+
+        SDL_Init(SDL_INIT_EVERYTHING);
+
+        std::string title = "ENTER TITLE HERE!";
+        window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED,
+                                  SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+
+        //-1 ger renderare som passar
+        renderer = SDL_CreateRenderer(window, -1, 0);
+
+        //Init på TTF om vi använder en font
+        //TTF_Init();
+
+        //Ta bort denna! Endast här för att testa att fönstret syns
+        SDL_Delay(5000);
+
+    }
+
+
+    System::~System() {
+
+        //Stäng fonten
+        //TTF_CloseFont(font);
+
+        //Avsluta TTF
+        //TTF_Quit();
+
+        SDL_DestroyWindow(window);
+        SDL_DestroyRenderer(renderer);
+
+        SDL_Quit();
+    }
+
+    SDL_Renderer* System::getRenderer() const {
+        return renderer;
+    }
+
+    //Get för fonten
+    //En global font som vi kan få fram genom att anropa sys.get_font();
+    //TTF_Font* System::get_font() const { return font;}
+
+    //Ligger utanför funktioner så objektet blir statiskt
+    System system;
+
+}
---
+++ src/System.h
@@ -0,0 +1,44 @@
+//
+// Created by Oskar Emilsson on 2017-11-30.
+//
+
+#ifndef SPEL_SYSTEM_H
+#define SPEL_SYSTEM_H
+
+
+#include <SDL.h>
+#include <SDL_ttf.h>
+
+namespace game {
+
+    class System {
+
+    public:
+
+        System();
+
+        ~System();
+
+        SDL_Renderer *getRenderer() const;
+
+        //Eventuellt en font också
+        //TTF_Font* getFont() const;
+
+
+    private:
+
+        SDL_Window *window;
+        SDL_Renderer *renderer;
+
+        //Eventuellt font
+        //TTF_Font* font;
+
+
+    };
+
+    //extern = Alla som inkluderar headerfilen får en
+    //deklaration att det finns någonstans ett objekt av typen System
+    extern System system;
+
+}
+#endif //SPEL_SYSTEM_H
